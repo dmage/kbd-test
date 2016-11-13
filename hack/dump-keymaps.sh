458c37e766fa5f804d0064a9520d0acb01bb0f0a
Oleg Bulatov <oleg@bulatov.me>
2016-11-13 20:11:08 +0300

    Add charsets to 8bit dumps
    Signed-off-by: Oleg Bulatov <oleg@bulatov.me>

--- hack/dump-keymaps.sh
+++ hack/dump-keymaps.sh
@@ -15,14 +15,24 @@ void_keymap() {
     seq 0 255 | sed -e "s/.*/keycode & =/"
 }
 
+dumper_flags_unicode() {
+    :
+}
+
+dumper_flags_8bit() {
+    local keymap=$1
+    sed -ne 's/^charset "\(.*\)"\( *#.*\)\?$/-c \1/p' "$keymap"
+}
+
 do_dump() {
     local outdir="$1"
+    local dumper_flags="$2"
     mkdir -p "./$outdir"
     find ./keymaps -type f -name "*.map" | sed -e "s#./keymaps/##" | while read -r f; do
         n=$(printf "%s" "$f" | tr -c "A-Za-z0-9.-" "_")
         void_keymap | $LOADKEYS -c -s >/dev/null
         if $LOADKEYS "./keymaps/$f" >/dev/null 2>"./$outdir/$n"; then
-            $DUMPKEYS -f >>"./$outdir/$n"
+            $DUMPKEYS -f $($dumper_flags "./keymaps/$f") >>"./$outdir/$n"
         else
             echo "please check $outdir/$n" >&2
         fi
@@ -36,7 +46,7 @@ printf "%s: " "$DUMPKEYS"; $DUMPKEYS -V
 ) >&2
 
 kbd_mode -u
-do_dump dump/unicode
+do_dump dump/unicode dumper_flags_unicode
 
 kbd_mode -a
-do_dump dump/8bit
+do_dump dump/8bit dumper_flags_8bit
